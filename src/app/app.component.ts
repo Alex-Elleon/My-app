5e970bcb676e7a718162c219c43a90c3a7e45ea1
LuisSoria31 <utm23090484@utma.edu.mx>
2025-03-05 12:54:30 -0600

    Ay imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EncabezadoComponent } from "./encabezado/encabezado.component";
 import { ContenidoComponent } from "./contenido/contenido.component";
-import { PieComponent } from "./pie/pie.component";
 import { CardProductosComponent } from './card-productos/card-productos.component';
 import { RegistroComponent } from "./registro/registro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EncabezadoComponent, ContenidoComponent, PieComponent, CardProductosComponent, RegistroComponent],
+  imports: [RouterOutlet, EncabezadoComponent, ContenidoComponent, CardProductosComponent, RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
